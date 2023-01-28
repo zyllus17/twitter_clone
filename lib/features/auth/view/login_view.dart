c0a70da44887f0d269c8337ffb650f1df3ed1941
zyllus17 <mrfhassan17@gmail.com>
2023-01-28 22:28:45 +0530

    Login UI

---
+++ lib/features/auth/view/login_view.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/view/signup_view.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
+import 'package:twitter_clone/theme/theme.dart';
+
+class LoginView extends StatefulWidget {
+  static route() => MaterialPageRoute(
+        builder: (context) => const LoginView(),
+      );
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    final appbar = UIConstants.appBar();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    @override
+    void dispose() {
+      super.dispose();
+      emailController.dispose();
+      passwordController.dispose();
+    }
+
+    return Scaffold(
+      appBar: appbar,
+      body: Center(
+        child: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              AuthField(
+                controller: emailController,
+                hintText: 'Email',
+              ),
+              const SizedBox(height: 25),
+              AuthField(
+                controller: passwordController,
+                hintText: 'Password',
+              ),
+              const SizedBox(height: 40),
+              Align(
+                alignment: Alignment.topRight,
+                child: RoundedSmallButton(
+                  onTap: () {},
+                  label: 'Done',
+                ),
+              ),
+              const SizedBox(height: 40),
+              RichText(
+                text: TextSpan(
+                  text: "Don't have an account?",
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                        text: ' Sign up',
+                        style: const TextStyle(
+                          color: Pallete.blueColor,
+                          fontSize: 16,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(
+                              context,
+                              SignUpView.route(),
+                            );
+                          }),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
