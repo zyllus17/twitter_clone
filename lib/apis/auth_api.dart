bc0e44351f95f04d1915bcef1579360073eb9fe5
zyllus17 <mrfhassan17@gmail.com>
2023-01-29 01:38:05 +0530

    Added authAPIProvider

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,7 +1,14 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/core/core.dart';
+import 'package:twitter_clone/core/providers.dart';
+
+final authAPIProvider = Provider((ref) {
+  final account = ref.watch(appwriteAccountProvider);
+  return AuthAPI(account: account);
+});
 
 abstract class IAuthAPI {
   FutureEither<model.Account> signup({
