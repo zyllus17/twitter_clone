8693b973a0f462a45e474ac2bec280c32d4be224
zyllus17 <mrfhassan17@gmail.com>
2023-01-29 02:41:42 +0530

    Added login function

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/apis/auth_api.dart';
 import 'package:twitter_clone/core/utils.dart';
+import 'package:twitter_clone/features/auth/view/login_view.dart';
+import 'package:twitter_clone/features/home/view/home_view.dart';
 
 final authControllerProvider =
     StateNotifierProvider<AuthController, bool>((ref) {
@@ -31,7 +33,26 @@ class AuthController extends StateNotifier<bool> {
     state = false;
     res.fold(
       (l) => showSnackBar(context, l.message),
-      (r) => print(r.email),
+      (r) {
+        showSnackBar(context, 'Account created! Please login.');
+        Navigator.push(context, LoginView.route());
+      },
     );
+  }
+
+  void login({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    state = true;
+    final res = await _authAPI.login(
+      email: email,
+      password: password,
+    );
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) {
+      Navigator.push(context, HomeView.route());
+    });
   }
 }
