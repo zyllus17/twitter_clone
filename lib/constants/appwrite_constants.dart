d722e06fed9421263196c0a077b895b27c2ace69
zyllus17 <mrfhassan17@gmail.com>
2023-01-28 21:29:50 +0530

    Constants files

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,14 @@
+class AppWriteConstants {
+  static const String databaseId = '63d5418f04c932c7787b';
+  static const String projectId = '63d53c8e80607077a36a';
+  static const String endPoint = 'http://localhost:80/v1';
+
+  // static const String usersCollection = '63cb8ae73960973b0635';
+  // static const String tweetsCollection = '63cbd6781a8ce89dcb95';
+  // static const String notificationsCollection = '63cd5ff88b08e40a11bc';
+
+  // static const String imagesBucket = '63cbdab48cdbccb6b34e';
+
+  // static String imageUrl(String imageId) =>
+  //     '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
+}
