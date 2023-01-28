22854b18a39a05d84f87a7814938f3bed2d494cd
zyllus17 <mrfhassan17@gmail.com>
2023-01-29 02:42:02 +0530

    Added login functionality

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/features/auth/view/signup_view.dart';
 import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
-class LoginView extends StatefulWidget {
+class LoginView extends ConsumerStatefulWidget {
   static route() => MaterialPageRoute(
         builder: (context) => const LoginView(),
       );
   const LoginView({super.key});
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  ConsumerState<LoginView> createState() => _LoginViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _LoginViewState extends ConsumerState<LoginView> {
   final appbar = UIConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -28,61 +30,74 @@ class _LoginViewState extends State<LoginView> {
     passwordController.dispose();
   }
 
+  void onLogin() {
+    ref.read(authControllerProvider.notifier).login(
+          email: emailController.text,
+          password: passwordController.text,
+          context: context,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
       appBar: appbar,
-      body: Center(
-        child: SingleChildScrollView(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              AuthField(
-                controller: emailController,
-                hintText: 'Email',
-              ),
-              const SizedBox(height: 25),
-              AuthField(
-                controller: passwordController,
-                hintText: 'Password',
-              ),
-              const SizedBox(height: 40),
-              Align(
-                alignment: Alignment.topRight,
-                child: RoundedSmallButton(
-                  onTap: () {},
-                  label: 'Done',
-                ),
-              ),
-              const SizedBox(height: 40),
-              RichText(
-                text: TextSpan(
-                  text: "Don't have an account?",
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                  children: [
-                    TextSpan(
-                        text: ' Sign up',
-                        style: const TextStyle(
-                          color: Pallete.blueColor,
-                          fontSize: 16,
+      body: isLoading
+          ? const Loader()
+          : Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      AuthField(
+                        controller: emailController,
+                        hintText: 'Email',
+                      ),
+                      const SizedBox(height: 25),
+                      AuthField(
+                        controller: passwordController,
+                        hintText: 'Password',
+                      ),
+                      const SizedBox(height: 40),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: RoundedSmallButton(
+                          onTap: onLogin,
+                          label: 'Done',
                         ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                              context,
-                              SignUpView.route(),
-                            );
-                          }),
-                  ],
+                      ),
+                      const SizedBox(height: 40),
+                      RichText(
+                        text: TextSpan(
+                          text: "Don't have an account?",
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: ' Sign up',
+                              style: const TextStyle(
+                                color: Pallete.blueColor,
+                                fontSize: 16,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                    context,
+                                    SignUpView.route(),
+                                  );
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
-        )),
-      ),
+            ),
     );
   }
 }
