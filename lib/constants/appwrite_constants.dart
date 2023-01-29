411bd3110a401c79026d940425befd6844ad3b48
zyllus17 <mrfhassan17@gmail.com>
2023-01-30 02:09:14 +0530

    Provided fix for Android emulator

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,6 +1,8 @@
 class AppWriteConstants {
   static const String databaseId = '63d5418f04c932c7787b';
   static const String projectId = '63d53c8e80607077a36a';
+  //ISSUE: App doesn't work on Android
+  //FIX: Replace localhost with IPAddress to make it work on Android
   static const String endPoint = 'http://localhost:80/v1';
   static const String usersCollection = '63d6c3b034080d114c0b';
   // static const String tweetsCollection = '63cbd6781a8ce89dcb95';
