0a949ae0a62656e4b1abe0255d03d65b12136f5c
zyllus17 <mrfhassan17@gmail.com>
2023-01-30 01:05:40 +0530

    Added usersCollection

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -2,8 +2,7 @@ class AppWriteConstants {
   static const String databaseId = '63d5418f04c932c7787b';
   static const String projectId = '63d53c8e80607077a36a';
   static const String endPoint = 'http://localhost:80/v1';
-
-  // static const String usersCollection = '63cb8ae73960973b0635';
+  static const String usersCollection = '63d6c3b034080d114c0b';
   // static const String tweetsCollection = '63cbd6781a8ce89dcb95';
   // static const String notificationsCollection = '63cd5ff88b08e40a11bc';
 
