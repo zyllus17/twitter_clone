dc4becd80766fd5ffc52adc38bd55957a8ab2f90
zyllus17 <mrfhassan17@gmail.com>
2023-01-31 01:27:22 +0530

    Added bottomTabBarPages

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/constants/constants.dart';
-import 'package:twitter_clone/theme/theme.dart';
+// import 'package:twitter_clone/features/explore/view/explore_view.dart';
+// import 'package:twitter_clone/features/notifications/views/notification_view.dart';
+// import 'package:twitter_clone/features/tweet/widgets/tweet_list.dart';
+import 'package:twitter_clone/theme/pallete.dart';
 
 class UIConstants {
   static AppBar appBar() {
@@ -14,4 +17,10 @@ class UIConstants {
       centerTitle: true,
     );
   }
+
+  static const List<Widget> bottomTabBarPages = [
+    // TweetList(),
+    // ExploreView(),
+    // NotificationView(),
+  ];
 }
